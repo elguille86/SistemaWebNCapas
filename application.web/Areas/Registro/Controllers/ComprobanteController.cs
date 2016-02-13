292102bc4ece1722c0cd02581bc3d3e4456604f9
elguille86 <g.rodriguez.p@hotmail.com>
2016-02-13 16:12:43 -0500

    Actualizar Factura

--- application.web/Areas/Registro/Controllers/ComprobanteController.cs
+++ application.web/Areas/Registro/Controllers/ComprobanteController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using application.web.Areas.Registro.Models;
+
 namespace application.web.Areas.Registro.Controllers
 {
     [Authorize]
@@ -19,6 +21,39 @@ namespace application.web.Areas.Registro.Controllers
         {
             ViewBag.TitlePag = "Registrar Comprobante";
             return View();
+        }
+        [HttpPost]
+        public JsonResult SaveOver(CabComprobante o) {
+
+            bool status = false;
+            if (ModelState.IsValid) {
+                using (BDGRPNETEntities1 dc = new BDGRPNETEntities1()) {
+
+                    TB_COMPROBANTE_CAB order = new TB_COMPROBANTE_CAB { 
+                    
+                        estab_serie = "117",
+                        cabe_fecha = o.cabe_fecha,
+                        cabe_ticket ="0111-01444",
+                        cabe_hora = DateTime.Now,
+                        cod_estab = "E16000000001",
+                        usu_docid_codigo = o.usu_docid_codigo,
+                        login_user = User.Identity.Name,
+                        Nro_Fua ="45454",
+                        Mont_Total = o.Mont_Total
+                    };
+                    foreach (var i in o.TB_COMPROBANTE_DET) {
+                        order.TB_COMPROBANTE_DET.Add(i);
+                    
+                    }
+                    dc.TB_COMPROBANTE_CAB.Add(order);
+                    dc.SaveChanges();
+                    status = true;
+                }
+                
+                status = true;
+            } 
+            else { status = false; }
+            return new JsonResult { Data = new { status = status } };
         }
     }
 }
---
+++ application.web/Areas/Registro/Models/CabComprobante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace application.web.Areas.Registro.Models
+{
+    public class CabComprobante
+    {
+
+        //public string estab_serie { get; set; }
+        public Nullable<System.DateTime> cabe_fecha { get; set; }
+        //public string cabe_ticket { get; set; }
+        //public Nullable<System.DateTime> cabe_hora { get; set; }
+        //public string cod_estab { get; set; }
+        //public int cod_comp { get; set; }
+        public string usu_docid_codigo { get; set; }
+        //public string login_user { get; set; }
+        //public string Nro_Fua { get; set; }
+        public Nullable<decimal> Mont_Total { get; set; }
+
+        public List<TB_COMPROBANTE_DET> TB_COMPROBANTE_DET { get; set; }
+
+    }
+}
